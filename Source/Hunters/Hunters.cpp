b49b835168589bff75d58944cd5f439ef356ebf6
crusher90 <rahulvallecha17@gmail.com>
2022-05-10 17:58:10 +0530

    Added Project Files.

---
+++ Source/Hunters/Hunters.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Hunters.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Hunters, "Hunters" );
