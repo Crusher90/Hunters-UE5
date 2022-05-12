67342fb19f4719d8a7de02ce55e777c2e819e5c6
crusher90 <rahulvallecha17@gmail.com>
2022-05-12 19:30:37 +0530

    Added Pickup actor class for player buff.

---
+++ Source/Hunters/Pickup.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pickup.h"
+#include "Kismet/GameplayStatics.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "Sound/SoundCue.h"
+#include "Aurora.h"
+
+APickup::APickup()
+{
+
+}
+
+void APickup::ItemSphereOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult) 
+{
+    Super::ItemSphereOverlapBegin(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+    if(OtherActor)
+    {
+        Aurora = Cast<AAurora>(OtherActor);
+        if(Aurora)
+        {
+            if(ItemSound)
+            {
+                UGameplayStatics::PlaySound2D(this, ItemSound);
+            }
+            if(ItemParticle)
+            {
+                UGameplayStatics::SpawnEmitterAtLocation(this, ItemParticle, GetActorLocation(), FRotator(0.f));
+            }
+            Aurora->IncrementCoin();
+            Destroy();
+        }
+    }
+}
+
+void APickup::ItemSphereOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) 
+{
+    Super::ItemSphereOverlapEnd(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex);
+}
---
+++ Source/Hunters/Pickup.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "Pickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HUNTERS_API APickup : public AItem
+{
+	GENERATED_BODY()
+
+	public:
+	APickup();
+
+	virtual void ItemSphereOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult) override;
+
+	virtual void ItemSphereOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadonly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	class UParticleSystem *ItemParticle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadonly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	class USoundCue *ItemSound;
+	
+};
